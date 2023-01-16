4927e44fba8f4730463f7037ba1ca871e225b084
ShermanKata <pvtlab@yandex.ru>
2023-01-16 22:14:23 +0500

    commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -0,0 +1,20 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    void addUser(User user);
+
+    void deleteUserById(int id);
+
+    void updateUser(User user);
+
+    User getUserById(int id);
+
+    User getUserByUsername(String username);
+
+    List<User> getListOfUsers();
+}
